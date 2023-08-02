0e51804fa98fee3cf4ca0f3dab77985f3eede4cf
erickunzel <kunzel.eric@gmail.com>
2023-08-02 10:05:54 -0300

    feat: created Header, Loading and Highlight components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,28 @@
-import { Groups } from '@screens/Groups';
-import { ThemeProvider } from 'styled-components';
-import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
-import { ActivityIndicator } from 'react-native'
-import theme from './src/theme'
-
+import { Groups } from "@screens/Groups";
+import { ThemeProvider } from "styled-components";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
+import theme from "./src/theme";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
-  })
+    Roboto_700Bold,
+  });
 
   return (
     <ThemeProvider theme={theme}>
-      {
-        hasLoadedFonts ? <Groups /> : <ActivityIndicator />
-      }
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {hasLoadedFonts ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
